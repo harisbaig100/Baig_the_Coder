03fe8eaeb770fa6043527c66af5e3b03814ecc0f
harisbaig100-dev <Haris.Baig@al-ghurair.com>
2024-12-30 15:57:36 +0500

    initial portfolio template

---
+++ utils/data/contactsData.js
@@ -0,0 +1,12 @@
+export const contactsData = {
+    email: 'harisbaig100@gmail.com',
+    phone: '+923359661486',
+    address: 'Pakistan',
+    github: 'https://github.com/harisbaig100',
+    facebook: 'https://www.facebook.com/baig.mhb/',
+    linkedIn: 'https://www.linkedin.com/in/m-haris-baig-88a634146/',
+    twitter: 'https://www.linkedin.com/in/m-haris-baig-88a634146/',
+    stackOverflow: 'https://stackoverflow.com/users/5392687/muhammad-haris-baig',
+    leetcode: "https://leetcode.com/u/harisbaig/",
+    devUsername: "muhammad_harisbaig_1268d",
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "M.Haris Baig",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is M.Haris Baig. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'harisbaig100@gmail.com',
+  phone: '+923359661486',
+  address: 'Pakistan',
+  github: 'https://github.com/harisbaig100',
+  facebook: 'https://www.facebook.com/baig.mhb/',
+  linkedIn: 'https://www.linkedin.com/in/m-haris-baig-88a634146/',
+  twitter: 'https://www.linkedin.com/in/m-haris-baig-88a634146/',
+  stackOverflow: 'https://stackoverflow.com/users/5392687/muhammad-haris-baig',
+  leetcode: "https://leetcode.com/u/harisbaig/",
+  devUsername: "muhammad_harisbaig_1268d",
+  resume: "https://drive.google.com/file/d/1swo6rMoSmYMqq8l4DSAZp02vGa2VHJxu/view?usp=sharing"
+}
