97a3f25a2842dd709fe9a98b9526ea6a48579071
harisbaig100-dev <Haris.Baig@al-ghurair.com>
2025-01-12 21:24:34 +0500

    animation on homepage

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -8,6 +8,8 @@ import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
 import { MdDownload } from "react-icons/md";
 import { RiContactsFill } from "react-icons/ri";
 import { SiLeetcode } from "react-icons/si";
+import AnimationLottie from "../../helper/animation-lottie";
+import homeAnimation from '../../../assets/lottie/development.json';
 
 function HeroSection() {
   return (
@@ -20,14 +22,14 @@ function HeroSection() {
         className="absolute -top-[98px] -z-10"
       />
 
-      <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
-        <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
-          <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
-            <span className=" text-pink-500">{personalData.name}</span>
-            <span className=" text-[#16f2b3]">{personalData.designation}</span>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div className="order-2 lg:order-1 flex flex-col items-start justify-center lg:max-w-[90%]">
+          <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[1.6rem] lg:leading-[2.5rem]">
+            <span className="text-pink-500">{personalData.name}</span>
+            <span className="text-[#16f2b3]">{personalData.designation}</span>
             <br />
             <br />
-            <span className=" text-[#16f2b3]">{personalData.designation2}</span>
+            <span className="text-[#16f2b3]">{personalData.designation2}</span>
           </h1>
 
           <div className="my-12 flex items-center gap-5">
@@ -75,165 +77,12 @@ function HeroSection() {
               <MdDownload size={16} />
             </Link>
           </div>
-
         </div>
-        <div className="order-1 lg:order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37]">
-          <div className="flex flex-row">
-            <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
-            <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
-          </div>
-          <div className="px-4 lg:px-8 py-5">
-            <div className="flex flex-row space-x-2">
-              <div className="h-3 w-3 rounded-full bg-red-400"></div>
-              <div className="h-3 w-3 rounded-full bg-orange-400"></div>
-              <div className="h-3 w-3 rounded-full bg-green-200"></div>
-            </div>
-          </div>
-          <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
-          <code className="font-mono text-xs md:text-sm lg:text-base">
-          <div className="blink">
-            <span className="mr-2 text-pink-500">const</span>
-            <span className="mr-2 text-white">developer</span>
-            <span className="mr-2 text-pink-500">=</span>
-            <span className="text-gray-400">{'{'}</span>
-          </div>
-          
-          <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
-            <span className="text-gray-400">{`'`}</span>
-            <span className="text-amber-300">M. Haris Baig</span>
-            <span className="text-gray-400">{`',`}</span>
-          </div>
-          
-          <div className="ml-4 lg:ml-8 mr-2">
-            <span className="text-white">skills:</span>
-            <span className="text-gray-400">{`['`}</span>
-            <span className="text-amber-300">React Native</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">Expo</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">TypeScript</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">Redux Toolkit</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">React Query</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">Firebase</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">GraphQL</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">Jest</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">Styled Components</span>
-            <span className="text-gray-400">{"'],"}</span>
-          </div>
-          
-          <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">hardWorker:</span>
-            <span className="text-orange-400">true</span>
-            <span className="text-gray-400">,</span>
-          </div>
-          
-          <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">quickLearner:</span>
-            <span className="text-orange-400">true</span>
-            <span className="text-gray-400">,</span>
-          </div>
-          
-          <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">problemSolver:</span>
-            <span className="text-orange-400">true</span>
-            <span className="text-gray-400">,</span>
-          </div>
 
-          <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">openSourceContributor:</span>
-            <span className="text-orange-400">true</span>
-            <span className="text-gray-400">,</span>
+        <div className="order-1 lg:order-2 flex justify-end items-center h-full min-h-[500px]">
+          <div className="w-[150%]">
+            <AnimationLottie animationPath={homeAnimation} />
           </div>
-
-          <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">reactNativeCommunityDeveloper:</span>
-            <span className="text-orange-400">true</span>
-            <span className="text-gray-400">,</span>
-          </div>
-
-          <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">passionateProductHandler:</span>
-            <span className="text-orange-400">true</span>
-            <span className="text-gray-400">,</span>
-          </div>
-
-          <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">mentorshipExperience:</span>
-            <span className="text-orange-400">3</span>
-            <span className="text-gray-400">+</span>
-            <span className="text-amber-300"> years</span>
-            <span className="text-gray-400">,</span>
-          </div>
-
-          <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">projectsDeployed:</span>
-            <span className="text-orange-400">25</span>
-            <span className="text-gray-400">+</span>
-            <span className="text-amber-300"> apps</span>
-            <span className="text-gray-400">,</span>
-          </div>
-          
-          <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-green-400">hireable:</span>
-            <span className="text-orange-400">function</span>
-            <span className="text-gray-400">{'() {'}</span>
-          </div>
-          
-          <div>
-            <span className="ml-8 lg:ml-16 mr-2 text-orange-400">return</span>
-            <span className="text-gray-400">{`(`}</span>
-          </div>
-          
-          <div>
-            <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
-            <span className="mr-2 text-white">passionateAboutCode</span>
-            <span className="text-amber-300">&amp;&amp;</span>
-          </div>
-          
-          <div>
-            <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
-            <span className="mr-2 text-white">coffeeToCodeRatio</span>
-            <span className="mr-2 text-amber-300">&gt;</span>
-            <span className="text-orange-400">0.5 </span>
-            <span className="text-amber-300">&amp;&amp;</span>
-          </div>
-          
-          <div>
-            <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
-            <span className="mr-2 text-white">debuggingSkills</span>
-            <span className="mr-2 text-amber-300">===</span>
-            <span className="text-amber-300">'Expert' </span>
-            <span className="text-amber-300">&amp;&amp;</span>
-          </div>
-
-          <div>
-            <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
-            <span className="mr-2 text-white">canFixProductionAt</span>
-            <span className="mr-2 text-amber-300">===</span>
-            <span className="text-amber-300">'3 AM'</span>
-          </div>
-          
-          <div>
-            <span className="ml-8 lg:ml-16 mr-2 text-gray-400">{`);`}</span>
-          </div>
-          
-          <div>
-            <span className="ml-4 lg:ml-8 text-gray-400">{`};`}</span>
-          </div>
-          
-          <div>
-            <span className="text-gray-400">{`};`}</span>
-          </div>
-        </code>
-
-        </div>
         </div>
       </div>
     </section>
