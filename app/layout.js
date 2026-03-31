923b86766f4a694cb35108e02ed40a4aaa461ce2
harisbaig100-dev <Haris.Baig@al-ghurair.com>
2026-04-01 01:03:08 +0500

    next js error fixed

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
 import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { personalData } from "@/utils/data/personal-data";
 import Footer from "./components/footer";
 import ScrollToTop from "./components/helper/scroll-to-top";
+import ToastProvider from "./components/helper/toast-provider";
 import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
@@ -65,7 +65,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToastContainer />
+        <ToastProvider />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
